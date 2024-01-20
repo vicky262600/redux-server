f9c69895b08b7a9908add55d8061b4bd5baa88f4
vicky262600 <vickyjoshi2604@gmail.com>
2024-01-20 14:45:25 -0500

    post APIs done

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
 
+// create a post
 router.post("/", async ( req, res) =>{
     const newPost = new Post(req.body)
     try{
@@ -9,4 +10,41 @@ router.post("/", async ( req, res) =>{
     }catch(err){
         res.status(400).json(err)
     }
+});
+
+// delete a post
+router.delete("/:id", async(req, res)=>{
+    const thePost = await Post.findById(req.params.id);
+    if(thePost.userId !== req.body.userId){
+        res.status(500).json("you can only delete your post")
+    }else{
+        await thePost.deleteOne();
+        res.status(200).json("the post has been deleted");
+    }
+});
+
+// like/dislike the post
+router.put("/:id/like", async (req, res) =>{
+    const post = await Post.findById(req.params.id);
+    const userId = req.body.userId;
+    if(post.like.includes(userId)){
+        await post.updateOne({$pull:{like: req.body.userId}});
+        res.status(200).json("you have disliked the post");
+
+    }else{
+        await post.updateOne({ $push:{like: req.body.userId}});
+        res.status(200).json("you have liked the post");
+    }
+});
+
+// get posts
+router.get("/timeline", async(req, res) =>{
+    try{
+        const allPosts = await Post.find();
+        res.status(200).json(allPosts);
+    }catch(err){
+        res.status(501).json("no post");
+    }
 })
+
+module.exports = router
